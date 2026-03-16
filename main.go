c9f97ab9dee95d5ed2c02d95da8d2afdd2dfc94c
kenneth <kennethwarui35@gmail.com>
2026-03-16 22:10:37 +0300

    added relationships and calling table

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"service-shopper/model"
+
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+func main(){
+	// form sqlite database
+	db, err := gorm.Open(
+		sqlite.Open("checker.db"),
+		&gorm.Config{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	err = db.AutoMigrate(&model.Customer{}, &model.Service{}, &model.Product{})
+	if err != nil{
+		log.Fatal(err)
+	}
+	
+}
--- model/model.go
+++ model/model.go
@@ -1,4 +1,4 @@
-package main
+package model
 
 import (
 	"gorm.io/gorm"
@@ -9,16 +9,27 @@ type Customer struct{
 
 	// one-to-many with Product
 	Products []Product`gorm:"foreignKey:CustomerID;"`
+
+	// many-to-many with service 
+	Services []Service`gorm:"many2many:customer_service;"`
 }
 
 type Service struct{
 	gorm.Model
 	Name string
+	Rating int
+
+	// one-to-many with Service
+	Products []Product`gorm:"foreignKey:ServiceID;"`
+
+	// many-to-many with customer 
+	Customers []Customer`gorm:"many2many:customer_service;"`
 }
 
 type Product struct{
 	gorm.Model
 	Item_Name string
 	
+	ServiceID uint // foreignKey to service 
 	CustomerID uint // foreignKey to customer 
 }
