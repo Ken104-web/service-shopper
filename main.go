feaab95b17ad0036ff8aacb1de430560c1c03799
kenneth <kennethwarui35@gmail.com>
2026-03-31 02:07:53 +0300

    seeded service and proceeding with product

--- main.go
+++ main.go
@@ -17,6 +17,7 @@ func main(){
 	if err != nil{
 		log.Fatal(err)
 	}
+
 	// insert customer data into tables 
 	// define the slice(literal)
 	customers := []model.Customer{}
@@ -34,7 +35,27 @@ func main(){
 		if r.Error != nil{
 			log.Fatalf("no record of customer: %v", r.Error)
 		}
-
+	service := []model.Service{}
+	// append that data 
+	fake := faker.New()
+	for i:=0; i<3; i++{
+		s := fake.Person()
+		// r := fake.RandomDigit()
+		servant := model.Service{
+			Name: s.Name()
+			// Rating: r.Rating()
+		}
+		servants =append(servants, servant)
+	}
+	// save to DB 
+	r :=db.Create(&service)
+	if r.Error != nil{
+		log.Fatal("customer cannot be served due to lack of servant: %v", r.Error)
+	}
+	
+	product := []model.Product{}
+	// list the array data
+	items := [...]string{"Bread", "Milk", "Eggs", "Fresh fruits"}
 
 	err = db.AutoMigrate(&model.Customer{}, &model.Service{}, &model.Product{})
 	if err != nil{
